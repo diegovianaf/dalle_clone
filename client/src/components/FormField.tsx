40d03743a60b04b4a73cfba13dc4339b04e05928
Diego Viana <diegovianaf7@gmail.com>
2023-02-27 16:23:51 -0300

    feat(components): add isSurpriseMe and handleSurpriseMe props to FormField component
    The FormField component now displays a button that generates a random prompt from the surpriseMePrompts array when clicked.

--- client/src/components/FormField.tsx
+++ client/src/components/FormField.tsx
@@ -1,6 +1,56 @@
-const FormField = () => {
+type FormFieldProps = {
+  labelName: string
+  type: string
+  name: string
+  placeholder: string
+  value: string
+  handleChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
+  isSurpriseMe?: boolean
+  handleSurpriseMe?: () => void
+}
+
+const FormField = (props: FormFieldProps) => {
+  const {
+    labelName,
+    type,
+    name,
+    placeholder,
+    value,
+    handleChange,
+    isSurpriseMe,
+    handleSurpriseMe
+  } = props
+
   return (
-    <div>FormField</div>
+    <div>
+      <div className='flex items-center gap-2 mb-2'>
+        <label
+          htmlFor={name}
+          className='block text-sm font-medium text-gray-900'
+        >
+          {labelName}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type='button'
+            onClick={handleSurpriseMe}
+            className='font-semibold text-xs bg-indigo-100 py-1 px-2 rounded text-black'
+          >
+            Surprise me
+          </button>
+        )}
+      </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 outline-none block w-full p-3'
+      />
+    </div>
   )
 }
 
