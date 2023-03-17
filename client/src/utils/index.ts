bf7ec72a36f8fb86a6696b96b41cb32f07b399b0
Diego Viana <diegovianaf7@gmail.com>
2023-03-17 14:51:26 -0300

    feat: add image downloading functionality
    - Update package.json to include @types/file-saver dependency
    - Add downloadImage function to utils/index.ts to enable image downloading

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,3 +1,5 @@
+import FileSaver from 'file-saver'
+
 import { surpriseMePrompts } from '../constants'
 
 export const getRandomPrompt = (prompt: string): string => {
@@ -7,4 +9,8 @@ export const getRandomPrompt = (prompt: string): string => {
   if (randomPrompt === prompt) return getRandomPrompt(prompt)
 
   return randomPrompt
+}
+
+export const downloadImage = async (_id: string, photo: string): Promise<void> => {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`)
 }
